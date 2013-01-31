3c75cfaa8b1d91f175bf42f4e8552f67524fd0d2
Phillip Pollard <bennie@cpan.org>
2013-01-31 00:58:01 +0000

    Skeleton build.

---
+++ lib/Parse/NetApp/ASUP.pm
@@ -0,0 +1,22 @@
+$Parse::NetApp::ASUP::VERSION='VERSIONTAG';
+
+use strict;
+
+package Parse::NetApp::ASUP;
+
+1;
+
+=head1 SYNOPSIS:
+
+Parse NetApp Weekly Auto Support Files
+
+=head1 USAGE:
+
+  use Parse::NetApp::ASUP;
+
+=head1 AUTHORSHIP:
+
+  Parse::NetApp::ASUP vVERSIONTAG DATETAG
+
+  (c) 2013-YEARTAG, Phillip Pollard <bennie@cpan.org>
+  Released under the Perl Artistic License
